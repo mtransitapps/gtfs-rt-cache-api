c003e9c608b42d1087e5775f7a5daa052432eb83
Mathieu Méa <mathieu.mea@gmail.com>
2025-10-09 11:54:40 -0400

    cleanup

--- src/index.js
+++ src/index.js
@@ -3,23 +3,21 @@ export default {
     console.log(`[MT]> request url: '${request.url}'.`);
 
     const requestUrl = new URL(request.url);
-    console.log(`[MT]> request url - host: '${requestUrl.host}'.`);
-    console.log(`[MT]> request url - origin: '${requestUrl.origin}'.`);
-    console.log(`[MT]> request url > search: '${requestUrl.search}'.`);
+    // console.log(`[MT]> request url - host: '${requestUrl.host}'.`);
+    // console.log(`[MT]> request url - origin: '${requestUrl.origin}'.`);
+    // console.log(`[MT]> request url > search: '${requestUrl.search}'.`);
     console.log(`[MT]> request url > pathname: '${requestUrl.pathname}'.`);
-    const search = requestUrl.search;
+    // const search = requestUrl.search;
     const pathname = requestUrl.pathname;
     const pathnameParts = pathname.split("/");
     console.log(`[MT]> request url > pathname parts[${pathnameParts.length}]: '${pathnameParts}'.`);
 
-    const supportedUsernames = [];
-    const supportedUserIds = [];
-    console.log(`[MT]> supported usernames: ${supportedUsernames.length}`);
-    console.log(`[MT]> supported user IDs: ${supportedUserIds.length}`);
-
-    // /ca_longueuil_rtl/service-alerts
-    // /ca_longueuil_rtl/trip-updates
-    // /ca_longueuil_rtl/vehicle-positions
+    // /xx_city_agency/service-alerts
+    // /xx_city_agency/trip-updates
+    // /xx_city_agency/vehicle-positions
+    const SERVICE_ALERTS = "service-alerts";
+    const TRIP_UPDATES = "trip-updates";
+    const VEHICLE_POSITIONS = "vehicle-positions";
 
     let cacheControl = '';
     cacheControl = 's-maxage=60'; // DEBUG
@@ -34,7 +32,7 @@ export default {
           if (pathnameParts.length > 2) {
             const urlType = pathnameParts[2];
             switch (urlType) {
-              case "service-alerts":
+              case SERVICE_ALERTS:
                 cacheControl = 's-maxage=30'; // minimum interval of 05 seconds between each call to our open data service.
                 apiUrl = 'https://rtl.chrono-saeiv.com/api/opendata/v1/RTL/alert?token='; 
                 apiUrlWithSecret = apiUrl + secret;
