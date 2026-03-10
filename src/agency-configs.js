2788f8e147351a8d4ff3f148074bb6f33a5f6329
Mathieu Méa <mathieu.mea@gmail.com>
2026-03-10 16:20:40 -0400

    PR comments

--- src/agency-configs.js
+++ src/agency-configs.js
@@ -11,7 +11,7 @@ export const getAgencyConfigs = (env) => ({
     serviceAlertsUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/MRCLASSO/alert?token=',
     serviceAlertsUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/MRCLASSO/alert?token=${env.MT_GTFS_RT_ca_l_assomption_mrclasso}`,
     tripUpdatesUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/MRCLASSO/tripupdate?token=',
-    tripUpdatesUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/MRCLASSO/tripupdate?token=${env.MT_GTFS_RT_ca_chambly_richelieu_carignan_citcrc}`,
+    tripUpdatesUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/MRCLASSO/tripupdate?token=${env.MT_GTFS_RT_ca_l_assomption_mrclasso}`,
     vehiclePositionsUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/MRCLASSO/vehicleposition?token=',
     vehiclePositionsUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/MRCLASSO/vehicleposition?token=${env.MT_GTFS_RT_ca_l_assomption_mrclasso}`
   },
@@ -19,7 +19,7 @@ export const getAgencyConfigs = (env) => ({
     serviceAlertsUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/CITPI/alert?token=',
     serviceAlertsUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/CITPI/alert?token=${env.MT_GTFS_RT_ca_la_presqu_ile_citpi}`,
     tripUpdatesUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/CITPI/tripupdate?token=',
-    tripUpdatesUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/CITPI/tripupdate?token=${env.MT_GTFS_RT_ca_chambly_richelieu_carignan_citcrc}`,
+    tripUpdatesUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/CITPI/tripupdate?token=${env.MT_GTFS_RT_ca_la_presqu_ile_citpi}`,
     vehiclePositionsUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/CITPI/vehicleposition?token=',
     vehiclePositionsUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/CITPI/vehicleposition?token=${env.MT_GTFS_RT_ca_la_presqu_ile_citpi}`
   },
@@ -27,7 +27,7 @@ export const getAgencyConfigs = (env) => ({
     serviceAlertsUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/CITLA/alert?token=',
     serviceAlertsUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/CITLA/alert?token=${env.MT_GTFS_RT_ca_laurentides_citla}`,
     tripUpdatesUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/CITLA/tripupdate?token=',
-    tripUpdatesUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/CITLA/tripupdate?token=${env.MT_GTFS_RT_ca_chambly_richelieu_carignan_citcrc}`,
+    tripUpdatesUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/CITLA/tripupdate?token=${env.MT_GTFS_RT_ca_laurentides_citla}`,
     vehiclePositionsUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/CITLA/vehicleposition?token=',
     vehiclePositionsUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/CITLA/vehicleposition?token=${env.MT_GTFS_RT_ca_laurentides_citla}`
   },
@@ -35,7 +35,7 @@ export const getAgencyConfigs = (env) => ({
     serviceAlertsUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/LRRS/alert?token=',
     serviceAlertsUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/LRRS/alert?token=${env.MT_GTFS_RT_ca_le_richelain_citlr}`,
     tripUpdatesUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/LRRS/tripupdate?token=',
-    tripUpdatesUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/LRRS/tripupdate?token=${env.MT_GTFS_RT_ca_chambly_richelieu_carignan_citcrc}`,
+    tripUpdatesUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/LRRS/tripupdate?token=${env.MT_GTFS_RT_ca_le_richelain_citlr}`,
     vehiclePositionsUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/LRRS/vehicleposition?token=',
     vehiclePositionsUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/LRRS/vehicleposition?token=${env.MT_GTFS_RT_ca_le_richelain_citlr}`
   },
@@ -43,7 +43,7 @@ export const getAgencyConfigs = (env) => ({
     serviceAlertsUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/MRCLM/alert?token=',
     serviceAlertsUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/MRCLM/alert?token=${env.MT_GTFS_RT_ca_les_moulins_mrclm}`,
     tripUpdatesUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/MRCLM/tripupdate?token=',
-    tripUpdatesUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/MRCLM/tripupdate?token=${env.MT_GTFS_RT_ca_chambly_richelieu_carignan_citcrc}`,
+    tripUpdatesUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/MRCLM/tripupdate?token=${env.MT_GTFS_RT_ca_les_moulins_mrclm}`,
     vehiclePositionsUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/MRCLM/vehicleposition?token=',
     vehiclePositionsUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/MRCLM/vehicleposition?token=${env.MT_GTFS_RT_ca_les_moulins_mrclm}`
   },
@@ -51,7 +51,7 @@ export const getAgencyConfigs = (env) => ({
     serviceAlertsUrl: 'https://rtl.chrono-saeiv.com/api/opendata/v1/RTL/alert?token=',
     serviceAlertsUrlWithSecret: `https://rtl.chrono-saeiv.com/api/opendata/v1/RTL/alert?token=${env.MT_GTFS_RT_ca_longueuil_rtl}`,
     tripUpdatesUrl: 'https://rtl.chrono-saeiv.com/api/opendata/v1/RTL/tripupdate?token=',
-    tripUpdatesUrlWithSecret: `https://rtl.chrono-saeiv.com/api/opendata/v1/RTL/tripupdate?token=${env.MT_GTFS_RT_ca_chambly_richelieu_carignan_citcrc}`,
+    tripUpdatesUrlWithSecret: `https://rtl.chrono-saeiv.com/api/opendata/v1/RTL/tripupdate?token=${env.MT_GTFS_RT_ca_longueuil_rtl}`,
     vehiclePositionsUrl: 'https://rtl.chrono-saeiv.com/api/opendata/v1/RTL/vehicleposition?token=',
     vehiclePositionsUrlWithSecret: `https://rtl.chrono-saeiv.com/api/opendata/v1/RTL/vehicleposition?token=${env.MT_GTFS_RT_ca_longueuil_rtl}`
   },
@@ -59,7 +59,7 @@ export const getAgencyConfigs = (env) => ({
     serviceAlertsUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/TRAINS/alert?token=',
     serviceAlertsUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/TRAINS/alert?token=${env.MT_GTFS_RT_ca_montreal_amt}`,
     tripUpdatesUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/TRAINS/tripupdate?token=',
-    tripUpdatesUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/TRAINS/tripupdate?token=${env.MT_GTFS_RT_ca_chambly_richelieu_carignan_citcrc}`,
+    tripUpdatesUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/TRAINS/tripupdate?token=${env.MT_GTFS_RT_ca_montreal_amt}`,
     vehiclePositionsUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/TRAINS/vehicleposition?token=',
     vehiclePositionsUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/TRAINS/vehicleposition?token=${env.MT_GTFS_RT_ca_montreal_amt}`
   },
@@ -73,7 +73,7 @@ export const getAgencyConfigs = (env) => ({
     serviceAlertsUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/CITVR/alert?token=',
     serviceAlertsUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/CITVR/alert?token=${env.MT_GTFS_RT_ca_richelieu_citvr}`,
     tripUpdatesUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/CITVR/tripupdate?token=',
-    tripUpdatesUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/CITVR/tripupdate?token=${env.MT_GTFS_RT_ca_chambly_richelieu_carignan_citcrc}`,
+    tripUpdatesUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/CITVR/tripupdate?token=${env.MT_GTFS_RT_ca_richelieu_citvr}`,
     vehiclePositionsUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/CITVR/vehicleposition?token=',
     vehiclePositionsUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/CITVR/vehicleposition?token=${env.MT_GTFS_RT_ca_richelieu_citvr}`
   },
@@ -81,7 +81,7 @@ export const getAgencyConfigs = (env) => ({
     serviceAlertsUrl: 'https://sts.chrono-saeiv.com/api/opendata/v1/STS/alert?token=',
     serviceAlertsUrlWithSecret: `https://sts.chrono-saeiv.com/api/opendata/v1/STS/alert?token=${env.MT_GTFS_RT_ca_sherbrooke_sts}`,
     tripUpdatesUrl: 'https://sts.chrono-saeiv.com/api/opendata/v1/STS/tripupdate?token=',
-    tripUpdatesUrlWithSecret: `https://sts.chrono-saeiv.com/api/opendata/v1/STS/tripupdate?token=${env.MT_GTFS_RT_ca_chambly_richelieu_carignan_citcrc}`,
+    tripUpdatesUrlWithSecret: `https://sts.chrono-saeiv.com/api/opendata/v1/STS/tripupdate?token=${env.MT_GTFS_RT_ca_sherbrooke_sts}`,
     vehiclePositionsUrl: 'https://sts.chrono-saeiv.com/api/opendata/v1/STS/vehicleposition?token=',
     vehiclePositionsUrlWithSecret: `https://sts.chrono-saeiv.com/api/opendata/v1/STS/vehicleposition?token=${env.MT_GTFS_RT_ca_sherbrooke_sts}`
   },
@@ -89,7 +89,7 @@ export const getAgencyConfigs = (env) => ({
     serviceAlertsUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/CITSV/alert?token=',
     serviceAlertsUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/CITSV/alert?token=${env.MT_GTFS_RT_ca_sorel_varennes_citsv}`,
     tripUpdatesUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/CITSV/tripupdate?token=',
-    tripUpdatesUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/CITSV/tripupdate?token=${env.MT_GTFS_RT_ca_chambly_richelieu_carignan_citcrc}`,
+    tripUpdatesUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/CITSV/tripupdate?token=${env.MT_GTFS_RT_ca_sorel_varennes_citsv}`,
     vehiclePositionsUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/CITSV/vehicleposition?token=',
     vehiclePositionsUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/CITSV/vehicleposition?token=${env.MT_GTFS_RT_ca_sorel_varennes_citsv}`
   },
@@ -97,7 +97,7 @@ export const getAgencyConfigs = (env) => ({
     serviceAlertsUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/OMITSJU/alert?token=',
     serviceAlertsUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/OMITSJU/alert?token=${env.MT_GTFS_RT_ca_ste_julie_omitsju}`,
     tripUpdatesUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/OMITSJU/tripupdate?token=',
-    tripUpdatesUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/OMITSJU/tripupdate?token=${env.MT_GTFS_RT_ca_chambly_richelieu_carignan_citcrc}`,
+    tripUpdatesUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/OMITSJU/tripupdate?token=${env.MT_GTFS_RT_ca_ste_julie_omitsju}`,
     vehiclePositionsUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/OMITSJU/vehicleposition?token=',
     vehiclePositionsUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/OMITSJU/vehicleposition?token=${env.MT_GTFS_RT_ca_ste_julie_omitsju}`
   },
@@ -105,7 +105,7 @@ export const getAgencyConfigs = (env) => ({
     serviceAlertsUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/CITSO/alert?token=',
     serviceAlertsUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/CITSO/alert?token=${env.MT_GTFS_RT_ca_sud_ouest_citso}`,
     tripUpdatesUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/CITSO/tripupdate?token=',
-    tripUpdatesUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/CITSO/tripupdate?token=${env.MT_GTFS_RT_ca_chambly_richelieu_carignan_citcrc}`,
+    tripUpdatesUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/CITSO/tripupdate?token=${env.MT_GTFS_RT_ca_sud_ouest_citso}`,
     vehiclePositionsUrl: 'https://exo.chrono-saeiv.com/api/opendata/v1/CITSO/vehicleposition?token=',
     vehiclePositionsUrlWithSecret: `https://exo.chrono-saeiv.com/api/opendata/v1/CITSO/vehicleposition?token=${env.MT_GTFS_RT_ca_sud_ouest_citso}`
   }
